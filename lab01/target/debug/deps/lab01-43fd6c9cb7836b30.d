dd8f2e344af34c32f555ca4c86f9e028892f30a8
TheOttoZone <ako415@hotmail.com>
2026-01-19 16:50:30 -0800

    ok i think i fixed it

---
+++ lab01/target/debug/deps/lab01-43fd6c9cb7836b30.d
@@ -0,0 +1,5 @@
+/home/aiden/CISS301/lab01/target/debug/deps/lab01-43fd6c9cb7836b30.d: src/main.rs
+
+/home/aiden/CISS301/lab01/target/debug/deps/liblab01-43fd6c9cb7836b30.rmeta: src/main.rs
+
+src/main.rs:
