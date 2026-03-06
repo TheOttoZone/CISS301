7a6ca74f29fceefc6665d2a614d34b4a96b1683e
TheOttoZone <ako415@hotmail.com>
2026-03-06 10:02:40 -0800

    idk what i did

--- lab07/src/main.rs
+++ lab07/src/main.rs
@@ -6,7 +6,6 @@ state_machine!{
     pub character_machine(Standing)
 
     Standing(Walk) => Walking,
-    Standing(Walk) => Walking,
     Standing(Jump) => Jumping,
     Standing(Lay) => Laying,
     Walking(Stop) => Standing,
@@ -60,19 +59,19 @@ struct Character{
 }
 
 impl Character {
-    pub fn new() -> Self {
+    pub fn new() -> Self { // character contsrtructor
         Self {
             machine: StateMachine::new(),
             direction: Direction::Right,
-            jump_counter: 0,
+            jump_counter: 0, // tracks how long the character has been in the air
             x: 0,
             y: 0,
         }
     }
-    pub fn input(&mut self, input_direction:Direction){
+    pub fn input(&mut self, input_direction:Direction){ // handle most state transitions from user input
         match input_direction{
             Direction::Up =>{
-                if self.machine.state() == &CharStates::Jumping{
+                if self.machine.state() == &CharStates::Jumping{ // jumping in air doesnt make sense
                     println!("You can't double jump!")
                 }else{
                     let _ = self.machine.consume(&CharInput::Jump);
@@ -82,58 +81,52 @@ impl Character {
                 let _ = self.machine.consume(&CharInput::Lay);
             }
             Direction::Left =>{
-                if self.direction == Direction::Right{
+                if self.direction == Direction::Right{ // handle turning right
                     let _ = self.machine.consume(&CharInput::Stop);
                     self.direction = Direction::Left;
                 }else{
-                    self.direction = Direction::Left;
                     let _ = self.machine.consume(&CharInput::Walk);
                 }
 
             },
             Direction::Right =>{
-                if self.direction == Direction::Left{
+                if self.direction == Direction::Left{ // handle turning left
                     let _ = self.machine.consume(&CharInput::Stop);
                     self.direction = Direction::Right
                 }else{
-                    self.direction = Direction::Right;
                     let _ = self.machine.consume(&CharInput::Walk);
                 }
             },
             
         }
     }
-    pub fn update(&mut self){
+    pub fn update(&mut self){ // game update logic
         if let CharStates::Walking = self.machine.state(){
             match self.direction{
-                Direction::Up => {}, // unused
-                Direction::Down => {}, // unsused
                 Direction::Left => {
                     self.x -= 1
                 },
                 Direction::Right => {
                     self.x += 1
                 },
-
+                _ => {}
             }
         }
         if let CharStates::Running = self.machine.state(){
             match self.direction{
-                Direction::Up => {}, // unused
-                Direction::Down => {}, // unsused
                 Direction::Left => {
                     self.x -= 2
                 },
                 Direction::Right => {
                     self.x += 2
                 },
-
+                _ => {}
             }
         }
         if let CharStates::Jumping = self.machine.state(){ 
             if self.jump_counter < 3{
                 self.y += 1;
-                self.jump_counter += 1
+                self.jump_counter += 1;
             }else{
                 let _ = self.machine.consume(&CharInput::Fall);
                 self.jump_counter = 0;
@@ -141,7 +134,10 @@ impl Character {
         }
         if let CharStates::Falling = self.machine.state(){
             if self.y > 0{
-                self.y -= 1
+                self.y -= 1;
+                if self.y == 0{
+                    let _ = self.machine.consume(&CharInput::Stop);
+                }
             }else{
                 let _ = self.machine.consume(&CharInput::Stop);
             }
@@ -155,7 +151,7 @@ impl Character {
 }
 
 fn main() {
-    println!("Welcome to the totally real game!");
+    println!("Welcome to the totally real game! Use W, A, S, D to move and press enter to send your input. Enter can also be used to simply continue.\n");
     let mut character = Character::new();
 
     while character.machine.state() != &CharStates::Dead{
@@ -166,10 +162,11 @@ fn main() {
         io::stdin().read_line(&mut input).expect("Failed to read input");
 
         // reduce input line to the first character to avoid having to deal with variable length input
-        if input.trim().len() > 1{
+        while input.trim().len() > 1{
             input.pop();
         }
 
+        // handle input
         match input.trim().to_lowercase().as_str(){
             "w" => {
                 character.input(Direction::Up);
@@ -190,4 +187,5 @@ fn main() {
         }
         character.update();
     }
+    println!("Your character tripped really really hard! Your charcter is dead.")
 }
